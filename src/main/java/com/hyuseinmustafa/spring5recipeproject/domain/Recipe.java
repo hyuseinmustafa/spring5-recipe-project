2fa10ccccc2e1cd0cf5998ec84aaa7ef16404222
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-01-05 12:03:43 +0000

    Added Category Entity.

--- src/main/java/com/hyuseinmustafa/spring5recipeproject/domain/Recipe.java
+++ src/main/java/com/hyuseinmustafa/spring5recipeproject/domain/Recipe.java
@@ -23,6 +23,11 @@ public class Recipe {
     private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -120,4 +125,11 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
