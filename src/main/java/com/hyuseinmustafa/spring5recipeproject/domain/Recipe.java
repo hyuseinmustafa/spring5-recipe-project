9d34d053747c2df0f678270ebde0035e5864d257
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-01-05 17:28:45 +0000

    Added Recipe to addIngredient.

--- src/main/java/com/hyuseinmustafa/spring5recipeproject/domain/Recipe.java
+++ src/main/java/com/hyuseinmustafa/spring5recipeproject/domain/Recipe.java
@@ -95,6 +95,12 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Set<Ingredient> getIngredients() {
         return ingredients;
     }
@@ -125,6 +131,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Category> getCategories() {
