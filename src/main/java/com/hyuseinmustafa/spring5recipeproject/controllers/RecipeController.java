3e7203e4a93bd51a8da0fc59b3be6d9f67ce5d64
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-01-13 21:48:28 +0000

    new recipe form and new recipe button added.

--- src/main/java/com/hyuseinmustafa/spring5recipeproject/controllers/RecipeController.java
+++ src/main/java/com/hyuseinmustafa/spring5recipeproject/controllers/RecipeController.java
@@ -1,10 +1,11 @@
 package com.hyuseinmustafa.spring5recipeproject.controllers;
 
+import com.hyuseinmustafa.spring5recipeproject.commands.RecipeCommand;
 import com.hyuseinmustafa.spring5recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
@@ -15,11 +16,18 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @GetMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @GetMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
     }
 }
